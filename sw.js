eed66b934c89673be097dcbeffa31d37d4ea7212
HandyNotdienst <valirikshimotyk@gmail.com>
2026-02-04 16:49:30 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "hn-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.webmanifest",
+  "./assets/logo.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
